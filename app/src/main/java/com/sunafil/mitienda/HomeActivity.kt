5a8f536a0c7d979747755a9acfc6ed054009fb94
bsoto24 <bsoto@intercorp.com.pe>
2022-12-11 21:13:51 -0500

    Add intent to open HomeActivity Add styles.xml

---
+++ app/src/main/java/com/sunafil/mitienda/HomeActivity.kt
@@ -0,0 +1,29 @@
+package com.sunafil.mitienda
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sunafil.mitienda.databinding.ActivityHomeBinding
+
+
+/**
+ * HomeActivity
+ *
+ * @author Bryam Soto - Interbank
+ * @email bsoto@intercorp.com.pe
+ * @since 7/12/22
+ */
+class HomeActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding // lateinit: inicializar la variable despues
+
+    //cuando se crea la pantalla y nos permite enlazar la vista
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater) //generar la pantalla
+        setContentView(binding.root) //enlazar la vista con nuestro activity
+
+
+    }
+
+
+}
--- app/src/main/java/com/sunafil/mitienda/MainActivity.kt
+++ app/src/main/java/com/sunafil/mitienda/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.sunafil.mitienda
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.sunafil.mitienda.databinding.ActivityMainBinding
 
-const val CORREO_EXITO = "bsoto.dev@gmail.com"
-const val PASSWORD_EXITO = "1234"
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,19 +14,61 @@ class MainActivity : AppCompatActivity() {
     // Se ejecuta cuando se crea la pantalla
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("MainActivity", "onCreate")
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnIngresar.setOnClickListener {
-            binding.btnIngresar.text = "Otro nombre"
+//            Log.e("MainActivity", "Le dio click al boton")
+//
+//
+//            binding.btnIngresar.text = "Otro nombre"
+//
+//            if (binding.edtCorreo.text.toString() == CORREO_EXITO && binding.edtPassword.text.toString() == PASSWORD_EXITO) {
+//                val intent: Intent = Intent(this, HomeActivity::class.java)
+//                startActivity(intent)
+//            } else {
+//                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show() //duracion corta
+//            }
 
-            if (binding.edtCorreo.text.toString() == CORREO_EXITO && binding.edtPassword.text.toString() == PASSWORD_EXITO) {
-                Toast.makeText(this, "Login satisfactorio", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
-            }
+            val intent: Intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("MainActivity", "onStart")
+    }
+
+    // cuando la pantalla es visible y es interactuable
+    override fun onResume() {
+        super.onResume()
+        Log.e("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("MainActivity", "onPause")
+    }
+
+    // para guardar datos
+    override fun onStop() {
+        super.onStop()
+        Log.e("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("MainActivity", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("MainActivity", "onRestart")
     }
 
 }
