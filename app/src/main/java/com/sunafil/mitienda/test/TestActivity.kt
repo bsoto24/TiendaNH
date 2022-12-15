f59c42b82a89cb89715648c9623d63fcf6c14047
bsoto24 <bsoto@intercorp.com.pe>
2022-12-14 20:24:08 -0500

    Agregamos TestActivity.kt Agregamos ProgressBar y SeekBar Agregamos listener para SeekBar y EditText

---
+++ app/src/main/java/com/sunafil/mitienda/test/TestActivity.kt
@@ -0,0 +1,60 @@
+package com.sunafil.mitienda.test
+
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.snackbar.Snackbar
+import com.sunafil.mitienda.databinding.ActivityTestBinding
+
+
+/**
+ * TestActivity
+ *
+ * @author Bryam Soto - Interbank
+ * @email bsoto@intercorp.com.pe
+ * @since 14/12/22
+ */
+class TestActivity: AppCompatActivity() {
+    
+    private lateinit var binding: ActivityTestBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+        initListener()
+    }
+
+    fun initUI(){
+
+    }
+
+    fun initListener(){
+        binding.seekbar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.btn.text = "${p1}"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                Snackbar.make(binding.root, "Selecciono: ${p0?.progress}", Snackbar.LENGTH_SHORT).show()
+            }
+
+        })
+
+        binding.btn.setOnClickListener {
+
+        }
+
+        binding.edt.doOnTextChanged { text, start, before, count ->
+            binding.btn.text = text
+        }
+    }
+
+}
